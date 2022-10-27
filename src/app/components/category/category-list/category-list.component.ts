63a2bf249fa6f2b51f9ce0574603d06f28c2c2ab
Ayman Elshiekh <a.elshiekh@najmtech.com>
2022-10-27 15:03:07 +0200

    load categories once

--- src/app/components/category/category-list/category-list.component.ts
+++ src/app/components/category/category-list/category-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { Category } from 'src/app/models/category';
 import { AppState } from 'src/app/state/app.state';
 import { deleteCategoryRequest, loadCategoriesRequest } from 'src/app/state/category/category.actions';
-import { selectCategories } from 'src/app/state/category/category.selectors';
+import { selectCategories, selectCategoryTotal } from 'src/app/state/category/category.selectors';
 
 @Component({
   selector: 'app-category-list',
@@ -15,11 +16,19 @@ export class CategoryListComponent implements OnInit {
 
   categories$: Observable<Category[]>;
   constructor(private store: Store<AppState>) {
-    this.store.dispatch(loadCategoriesRequest());
   }
 
   ngOnInit(): void {
+
+
+    this.store.pipe(take(1), select(selectCategoryTotal)).subscribe(total => {
+      if (total == 0)
+        this.store.dispatch(loadCategoriesRequest());
+    });
+
     this.categories$ = this.store.select(selectCategories);
+
+
   }
 
   delete(id) {
